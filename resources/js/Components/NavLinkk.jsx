731206acf0a51ede761c0204cf979e16981ba23d
nateeva <nataliaemerlo@gmail.com>
2024-07-15 20:46:50 -0300

    navbarLink component

---
+++ resources/js/Components/NavLinkk.jsx
@@ -0,0 +1,22 @@
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+const NavLinkk = ({ active = false, className = '', children, ...props }) => {
+  return (
+      <Link
+          {...props}
+          className={
+              'text-md font-semibold  transition duration-150 ease-in-out font-ebGaramond ' +
+              (active
+                  ? ' text-verde-100  '
+                  : ' text-marron-200 hover:text-verde-100  focus:text-verde-100') +
+              className
+          }
+      >
+          {children}
+      </Link>
+  )
+}
+
+export default NavLinkk
+
