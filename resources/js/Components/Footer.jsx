d60047a2ec73044036dd957f5b396cb94bd000cc
nateeva <nataliaemerlo@gmail.com>
2024-07-17 12:25:15 -0300

    cambios Footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     return (
         <div id='contacto' className='flex flex-col justify-center gap-16 py-24 md:flex-row md:justify-around bg-verde-100 font-ebGaramond text-marron-200 '>
             <div className='text-center'>
-                <h2 className='text-2xl font-bold'>Contacto</h2>
-                <ul className=''>
-                    <li><NavLink className='text-base font-normal hover:text-marron-200'>15 1234 6789</NavLink></li>
-                    <li><NavLink className='text-base font-normal hover:text-marron-200'>CABA, Arg.</NavLink></li>
+                <h2 className='mb-2 text-2xl font-bold md:text-3xl'>Contacto</h2>
+                <ul className='text-[18px] font-medium'>
+                    <li>15 1234 6789</li>
+                    <li>CABA, Arg.</li>
                 </ul>
             </div>
 
             <div className='text-center'>
-                <h2 className='text-2xl font-bold'>Social</h2>
-                <ul className=''>
-                    <li><NavLink className='text-base font-normal hover:text-marron-200'>instagram</NavLink></li>
-                    <li><NavLink className='text-base font-normal hover:text-marron-200'>whatsApp</NavLink></li>
+                <h2 className='mb-2 text-2xl font-bold md:text-3xl'>Social</h2>
+                <ul className='text-[18px] font-medium'>
+                    <li><a href="" className='hover:underline'>instagram</a></li>
+                    <li><a href="" className='hover:underline'>whatsApp</a></li>
                 </ul>
             </div>
 
