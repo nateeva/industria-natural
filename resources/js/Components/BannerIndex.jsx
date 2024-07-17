4bd3b73324e37fbbe3c4080598b351c1051055a7
nateeva <nataliaemerlo@gmail.com>
2024-07-17 10:49:01 -0300

    bg bannerIndex

--- resources/js/Components/BannerIndex.jsx
+++ resources/js/Components/BannerIndex.jsx
@@ -5,7 +5,14 @@ const BannerIndex = () => {
     const words1 = ["industria", "productos", "lorem"];
     const words2 = ["natural", "naturales", "ipsum"];
     return (
-        <div className=' bg-claro'>
+        <div className='bg-claro'
+            style={{
+                backgroundImage: "url('/images/bg_textura.png')",
+                backgroundRepeat:"no-repeat",
+                
+                }}
+            >
+
             <div className='grid items-center max-w-6xl md:grid-cols-2 mx-auto h-[90vh] md:h-[60vh] lg:h-[80vh] justify-items-center'>
                 <div className=''>
                     <TitlePrimary words1={words1} words2={words2} className="lg:w-[300px]" dynamic={true} />
