787c24c05d8a8c86cf2a3f6b8abc678bf42d4ef4
nateeva <nataliaemerlo@gmail.com>
2024-07-18 21:16:39 -0300

    cardProduct component

--- resources/js/Components/CardProduct.jsx
+++ resources/js/Components/CardProduct.jsx
@@ -13,23 +13,25 @@ const CardProduct = ({ title = "Título producto", price = 0 }) => {
     const formattedPrice = formatPriceToARS(price);
 
     return (
-        <div className='space-y-4 lg:max-w-sm'>
 
-            <img className='object-cover w-full h-full' src="images/product_index.png" alt={`Fotografía de producto ${title}`} />
-
-            <div className='w-full space-y-2 text-xl font-inter text-marron-200'>
-                <p>{title}</p>
-                <p>{formattedPrice}</p>
+        <div class="w-full md:max-w-sm bg-claro p-6">
+            <div className='h-64'>
+                <img class="object-cover h-full w-full" src="images/product_index_2.png" alt={`Fotografía de producto ${title}`} />
             </div>
 
+            <div class="py-5 text-marron-200 font-inter">
+
+                <h2 class="mb-2 text-2xl font-bold tracking-tight">{title}</h2>
+
+                <p class="mb-3">{formattedPrice}</p>
+
+            </div>
             <div className='w-full'>
                 <Link href="/tienda">
                     <Button className="">ver más</Button>
                 </Link>
 
             </div>
-
-
         </div>
     )
 }
